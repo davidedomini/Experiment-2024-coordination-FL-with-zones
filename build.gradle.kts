4e9c51977e418b477737bc63ffd6b374c547b43c
dom <davide.domini@unibo.it>
2024-02-19 15:53:19 +0100

    build: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(libs.scala2)
+    implementation(libs.scalapy)
+    implementation(libs.alchemist)
+    implementation(libs.alchemistGui)
+    implementation(libs.alchemistScafi)
 }
 
 tasks.test {
