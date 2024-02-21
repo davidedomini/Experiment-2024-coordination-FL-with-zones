ab3284c67ea4e06ad8acc72361e7434b4df350bf
dom <davide.domini@unibo.it>
2024-02-21 18:19:49 +0100

    feat: add aggregate algorithm

--- src/main/scala/it/unibo/SelfOrganisingCoordinationRegions.scala
+++ src/main/scala/it/unibo/SelfOrganisingCoordinationRegions.scala
@@ -1,11 +1,10 @@
 package it.unibo
 
-import scala.io.Source
 import it.unibo.alchemist.model.implementations.nodes.SimpleNodeManager
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 import de.siegmar.fastcsv.reader.CsvReader
-
-import java.io.{File, FileInputStream, InputStream}
+import it.unibo.PythonModules._
+import me.shadaj.scalapy.py
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -15,34 +14,34 @@ class SelfOrganisingCoordinationRegions
     with StandardSensors
     with ScafiAlchemistSupport {
 
-  private val radius = 1 // TODO - update
+  private val radius = 200 // TODO - update
+  private val localModel: py.Dynamic = utils.init_local_model()
+  private val every = 5 // TODO - check
 
   override def main(): Unit = {
     node.put("id", mid())
-    node.put("Code", loadData())
-
-    /*val initialModel: Int = mid()
-    val leader = S(radius, nbrRange)
-    val potential = distanceTo(leader)
-    if (leader) node.put("Leader", true)
-
-    rep((initialModel, 0)) { p =>
-      val localModel = p._1
+    val eoiCode = findStationCode() // TODO - check code name
+    val data: py.Dynamic = utils.get_dataset(eoiCode)
+    val aggregators = S(radius, nbrRange)
+    rep((localModel, 0)) { p => // TODO - add evaluation and logging
+      val model = p._1
       val tick = p._2
-      val models = collectIntoSet(potential, localModel)
-      val aggregateModel = averageWeights(models)
-      val zoneModel = broadcast(leader, aggregateModel)
-      broadcast(leader, zoneModel)
-      node.put("ID", mid())
-      node.put("Zone model", zoneModel)
-      (zoneModel, tick + 1)
-    }*/
-
+      val evolvedModel = evolve(model, data)
+      val potential = classicGradient(aggregators)
+      val info = C[Double, Set[py.Dynamic]](potential, _ ++ _, Set(evolvedModel), Set.empty)
+      val aggregatedModel = averageWeights(info)
+      val sharedModel = broadcast(aggregators, aggregatedModel)
+        mux(impulsesEvery(tick)){ (averageWeights(Set(sharedModel, evolvedModel)), tick+1) } { (evolvedModel, tick+1) }
+    }
   }
 
-  //private def averageWeights(models: Set[Int]): Int = (models.sum / models.size).toInt
+  private def averageWeights(models: Set[py.Dynamic]): py.Dynamic = ???
+  private def evolve(model: py.Dynamic, data: py.Dynamic): py.Dynamic = ???
+  private def evaluate(model: py.Dynamic, data: py.Dynamic): Int = ???
 
-  private def loadData()= {
+  private def impulsesEvery(time: Int): Boolean = time % every == 0
+
+  private def findStationCode()= {
     val n = node.asInstanceOf[SimpleNodeManager[Any]].node
     val pos = alchemistEnvironment.getPosition(n)
     val lat = BigDecimal(pos.getCoordinate(0)).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble.toString
